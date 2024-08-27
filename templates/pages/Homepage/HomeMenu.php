b9ab5b0aad78862f1df61b03286f2f68af10a0ea
Dodes <dvvr1013@users.noreply.github.com>
2024-08-27 19:16:02 +0800

    main menu

---
+++ templates/pages/Homepage/HomeMenu.php
@@ -0,0 +1,23 @@
+<style>
+  img{
+    border-radius: 50%;
+    border: 1px solid black;
+    background-color: blue;
+  }
+  .col-4{
+    column-count: 4;
+    column-gap: 50px;
+    position: absolute;
+    left: 250px;
+    top: 350px;
+   }
+</style>   
+    <div class="col-4">
+            <ul class="inline-list">
+                <li><img src="/hrproject_fe/images/ee_recentry.png" style="width:250px"/>Employee Record Entry</li>
+                <li><img src="/hrproject_fe/images/ee_timekeep.png" style="width:250px"/>Employee Time Keeping</li>
+                <li><img src="/hrproject_fe/images/ee_leave.png" style="width:250px"/>Employee Leave Application</li>
+                <li><img src="/hrproject_fe/images/ee_ot.png" style="width:250px"/>Employee Overtime Application</li>
+            </ul>
+     </div>
+    </div>
